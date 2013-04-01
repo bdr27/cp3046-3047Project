697966dc10f6238327b1daae3765bb09c799237d
Brendan Russo <brendanrusso27@gmail.com>
2013-04-01 22:01:36 +1000

    Finished a basic GUI walkthrough for adding players. Need to do the second screen and projector now

--- NerfWarsPrototype1/NerfWarsPrototype1/App.xaml.cs
+++ NerfWarsPrototype1/NerfWarsPrototype1/App.xaml.cs
@@ -23,13 +23,31 @@ namespace NerfWarsPrototype1
             mainWindow.regMenu.btnAddPlayer.Click += btnAddPlayer_Click;
             mainWindow.regAddPlayer.btnDone.Click += btnDone_Click;
             mainWindow.regMenu.btnAddTeam.Click += btnAddTeam_Click;
+            mainWindow.regAddTeam.btnDone.Click += btnDoneAddTeam_Click;
+            mainWindow.regAddTeam.btnAddPlayer.Click += btnAddPlayerTeam_Click;
             
             Console.WriteLine(mainWindow.regMenu.Visibility.ToString());
         }
 
-        void btnAddTeam_Click(object sender, RoutedEventArgs e)
+        private void btnAddPlayerTeam_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.regAddTeam.Visibility = Visibility.Hidden;
+            mainWindow.regAddPlayer.Visibility = Visibility.Visible;
+            //0 is home page
+            mainWindow.regAddPlayer.pageBefore = 1;
+        }
+
+        private void btnDoneAddTeam_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.regAddTeam.Visibility = Visibility.Hidden;
+            mainWindow.regMenu.Visibility = Visibility.Visible;
+        }
+
+        private void btnAddTeam_Click(object sender, RoutedEventArgs e)
         {
             mainWindow.regMenu.Visibility = Visibility.Hidden;
+            mainWindow.regAddTeam.Visibility = Visibility.Visible;
+            mainWindow.regAddPlayer.pageBefore = 1;
         }
 
         private void btnDone_Click(object sender, RoutedEventArgs e)
@@ -38,6 +56,10 @@ namespace NerfWarsPrototype1
             if (mainWindow.regAddPlayer.pageBefore == 0)
             {
                 mainWindow.regMenu.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                mainWindow.regAddTeam.Visibility = Visibility.Visible;
             }
         }
 
